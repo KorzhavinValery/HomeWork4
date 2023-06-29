fc75ce25e7c23bf50d109fc170c78708f936e1e6
Vel <korzhavin.vel@yandex.ru>
2023-06-29 17:54:02 +0700

    HomeWork4 Task4-5

--- src/Main.java
+++ src/Main.java
@@ -44,10 +44,51 @@ public class Main {
 
     public static void task4() {
         System.out.println("Задача 4");
+        int age = 10;
+        boolean canGoToSleep = age < 2;
+        boolean canGoKinderGarten = age >= 2 && age <= 6;
+        boolean canGoToSchool = age >= 7 && age <= 18;
+        boolean canGoToUniversity = age > 18 && age < 24;
+        boolean canGoToJob = age > 24;
+        boolean canGoToRest = age > 60;
+        if (canGoToSleep) {
+            System.out.println("Возраст человека равен " + age + ", ему пора поспать.");
+        } else {
+            System.out.println("Можно не идти спать");
+        }
+
+        if (canGoKinderGarten) {
+            System.out.println("Возраст человека равен " + age + ", ему нужно ходить в детский сад.");
+        }
+
+        if (canGoToSchool) {
+            System.out.println("Возраст человека равен " + age + ", ему нужно ходит в школу.");
+        }
+
+        if (canGoToUniversity) {
+            System.out.println("Возраст человека равен " + age + ", его место в университете.");
+        }
+        if (canGoToJob) {
+            System.out.println("Возраст человека равен " + age + ", пора ходить на работу");
+        }
+        if (canGoToRest) {
+            System.out.println("Возраст человека равен " + age + ", можно отдохнуть.");
+        }
+
     }
 
     public static void task5() {
         System.out.println("Задача 5");
+        int age = 4;
+        boolean childAccompaniedByAnAdult = age >= 5 && age < 14;
+        if (age < 5) {
+            System.out.println("Возраст ребёнка равен " + age + ", то ему нельзя кататься на аттракционе");
+
+        } else if (childAccompaniedByAnAdult) {
+            System.out.println("Возраст ребёнка равен " + age + ", можно кататься на аттракционе в сопровождении взрослого.");
+        } else {
+            System.out.println("Возраст ребёнка равен " + age + ", то ему можно кататься на аттракционе");
+        }
     }
 
     public static void task6() {
