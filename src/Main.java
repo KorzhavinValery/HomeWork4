3ce6bc09256f675d778059518f938d7e1b0bb8e2
Vel <korzhavin.vel@yandex.ru>
2023-06-29 15:38:20 +0700

    HomeWork4 Task1-3

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,35 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Человек достиг совершеннолетия его возраст равен " + age + " лет.");
+        } else {
+            System.out.println("Человек не достиг совершеннолетия, его возраст равен " + age + "," +
+                    "  нужно немного подождать.");
+        }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-
+        int temperature = 10;
+        if (temperature >= 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+        }
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-
+        int speed = 60;
+        if (speed <= 60) {
+            System.out.println("Ваша скорость составляет " + speed + " км/ч, превышения скорости нет," +
+                    " можно ездить спокойно.");
+        } else {
+            System.out.println("Ваша скорость составляет" + speed + " км/ч, скорость превышена, " +
+                    "придется заплатить штраф.");
+        }
     }
 
     public static void task4() {
